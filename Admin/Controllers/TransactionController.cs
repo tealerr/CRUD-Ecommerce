2ae85ee576062135074142c0ae61410c26e7e073
Teeramate Kantima <teeramet@dosetech.co>
2025-02-19 17:42:13 +0700

    [feat]: add api for get transaction list, get transaction by id

--- Admin/Controllers/TransactionController.cs
+++ Admin/Controllers/TransactionController.cs
@@ -1,3 +1,4 @@
+using Common.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +10,54 @@ namespace Admin.Controllers
     public class TransactionController : ControllerBase
     {
         // POST: api/transaction/list
-        [HttpPost("list")]
-        public IActionResult GetTransactions()
+        [HttpPost("transactions")]
+        public IActionResult GetTransactions([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
         {
 
-            return NoContent();
+            TransactionRepositories repository = new();
+
+            if (pageNumber < 1 || pageSize < 1)
+            {
+                return BadRequest("Page number and page size must be greater than zero.");
+            }
+
+            var tx = repository.GetTransactionList(pageNumber, pageSize);
+            if (tx == null || tx.Count == 0)
+            {
+                return NotFound("No transactions found.");
+            }
+
+            return Ok(new
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalTransactions = tx.Count,
+                Transactions = tx
+            });
         }
 
         // GET: api/transaction/{id}
-        [HttpGet("{id}")]
-        public IActionResult GetTransaction(int id)
+        [HttpGet("transaction/{transactionId}")]
+        public IActionResult GetTransactionByID(string transactionId)
         {
-            return NoContent();
+            if (string.IsNullOrWhiteSpace(transactionId))
+            {
+                return BadRequest("Product ID is required.");
+            }
+
+            TransactionRepositories repository = new();
+
+            var transaction = repository.GetUserTransactionByID(transactionId);
+
+            if (transaction == null)
+            {
+                return NotFound($"Transaction with ID '{transactionId}' not found.");
+            }
+
+            return Ok(new
+            {
+                results = transaction
+            });
         }
     }
 }
